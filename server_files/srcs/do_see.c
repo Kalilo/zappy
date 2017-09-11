28a40bfa7653aab4a2ddddbc90f5417be9732c28
Kalilo <kalilohansmann@gmail.com>
2017-09-11 17:16:16 +0200

    Saving progress.

--- server_files/srcs/do_see.c
+++ server_files/srcs/do_see.c
@@ -12,7 +12,7 @@
 
 #include "../includes/server.h"
 
-char	*sum_time_content(t_tile *tile)
+char	*sum_tile_content(t_tile *tile)
 {
 	char	*str;
 	int		k;
@@ -36,5 +36,50 @@ char	*sum_time_content(t_tile *tile)
 	cli = tile->players;
 	while (cli && ((cli = cli->next) || 1))
 		ft_str_append(&str, " player");
+	return (str);
+}
+
+/*
+** Need to rewrite usiing direction to update the position
+** Current method can't account for wrapping around the edge
+*/
+char	*sum_range(int x_min, int y_min, int x_max, int y_max)
+{
+	char	*str;
+	char	*tmp;
+
+	str = sum_tile_content(&MAP(x_min, y_min));
+	while (1)
+	{
+		if (x_min == x_max && y_min == y_max)
+			break ;
+		else if (x_min < x_max)
+			x_min++;
+		else if (x_min > x_max)
+			x_min--;
+		else if (y_min < y_max)
+			y_min++;
+		else if (y_min > y_max)
+			y_min--;
+		ft_str_append(&str, ", ");
+		tmp = sum_tile_content(&MAP(x_min, y_min));
+		str = ft_str_append3(&str, &tmp);
+	}
+	return (str);
+}
+
+char	*get_in_sight(t_client *client)
+{
+	int		level;
+	char	*str;
+	char	*tmp;
+
+	level = client->level;
+	str = ft_strdup("{");
+	while (level-- >= 0)
+	{
+		//
+	}
+	ft_str_append(&str, "}");
 	return (str);
 }

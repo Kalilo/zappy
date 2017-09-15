9ed0adf134b8ea62a83ff66386432b2bb5080dd0
Kalilo <kalilohansmann@gmail.com>
2017-09-15 15:35:40 +0200

    Added client ids

--- server_files/srcs/accept_new_client.c
+++ server_files/srcs/accept_new_client.c
@@ -14,14 +14,17 @@
 
 void	accept_new_client(void)
 {
-	int		sock;
+	int			sock;
+	static int	id;
+	t_client	*client;
 
 	if ((sock = accept(MASTER_SOCK, (t_sock_addr *)&MASTER_ADDR,
 			(socklen_t *)&MASTER_ADDRLEN)) < 0)
 		error_quit("Failed to accept new client");
 	ft_putendl("New client connected.");
-	if (!new_client(sock))
+	if (!(client = new_client(sock)))
 		error_quit("Failed to create new client");
+	client->id = ++id;
 	if (write_msg_to_sock(sock, "WELCOME\n") < 1)
 		error_quit("Failed to accept client connection");
 }

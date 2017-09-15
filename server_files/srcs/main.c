030c8dafc2f5a5ba29a827d5b8231ee781a430b7
Kalilo <kalilohansmann@gmail.com>
2017-09-15 09:10:50 +0200

    Split function

--- server_files/srcs/main.c
+++ server_files/srcs/main.c
@@ -13,35 +13,41 @@
 #define SERVER_MAIN
 #include "../includes/server.h"
 
-void	main_loop(void)
+void	tend_clients(struct timeval reff)
 {
 	struct timeval	now;
-	struct timeval	reff;
 	int				rem;
+
+	gettimeofday(&now, NULL);
+	rem = abs(reff.tv_usec - now.tv_usec);
+	while (rem < (1000000 / g_env.settings.fps))
+	{
+		prep_client_sockets();
+		reff.tv_sec = (g_env.settings.fps == 1) ? 1 : 0;
+		reff.tv_usec = (g_env.settings.fps == 1) ? 0 : 1000000 / \
+			g_env.settings.fps - rem;
+		ACTIVE_SOCK = select(MASTER_MAX_SD + 1, &READ_FDS, NULL, NULL, \
+			&reff);
+		if (ACTIVE_SOCK < 0)
+			error_quit("Failed to monitor sockets.");
+		if (FD_ISSET(MASTER_SOCK, &READ_FDS))
+			accept_new_client();
+		if (ACTIVE_SOCK > 0)
+			manage_clients();
+		gettimeofday(&now, NULL);
+		rem = abs(reff.tv_usec - now.tv_usec);
+	}
+}
+
+void	main_loop(void)
+{
+	struct timeval	reff;
 
 	while (1)
 	{
 		gettimeofday(&reff, NULL);
 		run_iteration();
-		gettimeofday(&now, NULL);
-		rem = abs(reff.tv_usec - now.tv_usec);
-		while (rem < (1000000 / g_env.settings.fps))
-		{
-			prep_client_sockets();
-			reff.tv_sec = (g_env.settings.fps == 1) ? 1 : 0;
-			reff.tv_usec = (g_env.settings.fps == 1) ? 0 : 1000000 / \
-				g_env.settings.fps - rem;
-			ACTIVE_SOCK = select(MASTER_MAX_SD + 1, &READ_FDS, NULL, NULL, \
-				&reff);
-			if (ACTIVE_SOCK < 0)
-				error_quit("Failed to monitor sockets.");
-			if (FD_ISSET(MASTER_SOCK, &READ_FDS))
-				accept_new_client();
-			if (ACTIVE_SOCK > 0)
-				manage_clients();
-			gettimeofday(&now, NULL);
-			rem = abs(reff.tv_usec - now.tv_usec);
-		}
+		tend_clients(reff);
 	}
 }
 

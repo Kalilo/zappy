393861fd35d3a736d5521fcc4dd22881ae5aae7d
Kalilo <kalilohansmann@gmail.com>
2017-09-21 17:34:32 +0200

    Neatened

--- client_files/ai.rb
+++ client_files/ai.rb
@@ -13,6 +13,7 @@ class AI
 
 	def find_food
 		puts "in AI::find_food" if @@verbose
+
 		begin
 			sight = self.see
 			self.right && (sight = self.see) unless sight.include? 'food'
@@ -20,6 +21,7 @@ class AI
 			self.right && (sight = self.see) unless sight.include? 'food'
 			@player.level.times { advance } unless sight.include? 'food'
 		end while !(sight.include? 'food')
+
 		path = @player.path_to('food')
 		@server.execute_list path[:path]
 		self.take 'food'
@@ -28,24 +30,28 @@ class AI
 
 	def advance
 		puts "in AI::advance" if @@verbose
+
 		@server.puts 'advance'
 		@player.advance
 	end
 
 	def left
 		puts "in AI::left" if @@verbose
+
 		@server.puts 'left'
 		@player.left
 	end
 
 	def right
 		puts "in AI::right" if @@verbose
+
 		@server.puts 'right'
 		@player.right
 	end
 
 	def see
 		puts "in AI::see" if @@verbose
+
 		@server.puts 'see'
 		s = nil
 		while s.nil? do
@@ -57,23 +63,27 @@ class AI
 
 	def take(resource)
 		puts "in AI::take('#{resource}')" if @@verbose
+
 		@server.puts "take #{resource}"
 		@player.take resource
 	end
 
 	def put(resource)
 		puts "in AI::put('#{resource}')" if @@verbose
+
 		@server.puts "put #{resource}"
 		@player.put resource
 	end
 
 	def broadcast(message)
 		puts "in AI::broadcast('#{message}')" if @@verbose
+
 		@server.puts "broadcast #{message}"
 	end
 
 	def look_for_resources
 		puts "in AI::look_for_resources" if @@verbose
+
 		required = @player.required_res
 		pos = @player.current_pos
 		current_required = required.select { |item| pos.include? item.to_s }
